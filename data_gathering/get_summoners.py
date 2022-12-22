66028223503b83b68c52355e8a2a723e8d61f37f
Clément Chauvet <83658432+ClementChauvet@users.noreply.github.com>
2022-12-22 18:24:35 +0100

    initial commit

---
+++ data_gathering/get_summoners.py
@@ -0,0 +1,128 @@
+
+
+
+
+import requests
+import json
+from tqdm import tqdm
+import time
+
+dirname = os.path.dirname(__file__)
+
+
+api_key = "RGAPI-26afc807-7c94-4fa2-9120-56657ca8abb5"
+nb_pages = 10
+
+region = "euw1"
+ranks = ["IRON", "BRONZE", "SILVER", "GOLD", "PLATINUM", "DIAMOND"]
+divs = ["IV", "III", "II", "I"]
+
+
+class API():
+    def __init__(self,api_key):
+        self.api_key = api_key
+        try:
+            os.chdir(os.path.dirname(__file__))
+        except NameError:
+            pass
+        
+    def get_from_api(self,url, api_key_given = False):
+        x = None
+        while x == None or x.status_code == 429:
+            time.sleep(1)
+            if api_key_given:
+                x = requests.get(url)
+            else:
+                x= requests.get(url + self.api_key)
+        if x.status_code == 403:
+            self.api_key = input("Time to refresh the key")
+            if api_key_given: 
+                return -1
+            x = requests.get(url + self.api_key)
+        if x.status_code != 200:
+            raise Exception("Error request got bad status code " + str(x.status_code) + " with message " + str(x.content) + "from url" + x.url)
+        y = json.loads(x.content)
+        return y
+    
+    def get_match_infos(self, match_id, region_alias):
+        dic = {}
+        url = "https://" + region_alias + ".api.riotgames.com/lol/match/v5/matches/" +match_id + "?api_key="
+        y = self.get_from_api(url)
+        participants = y["info"]["participants"]
+        dic["win"] = participants[0]["win"]
+        for p in range(len(participants)):
+            dic["summonerId_"+str(p)] = participants[p]["summonerId"]
+            dic["champ_" + str(p)] = participants[p]["championId"]
+        return dic
+        
+    
+    def get_match_ids(self, puuids, region_alias, games_by_player = 5, queue = 420, verbose = False):
+        match_ids = []
+        for puuid in tqdm(puuids, disable = not verbose):
+            url = "https://" + region_alias + ".api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids?queue=" + str(queue) + "&start=0&count=" + str(games_by_player) + "&api_key="
+            y = self.get_from_api(url)
+            match_ids += y
+        return match_ids
+            
+    def get_mastery(self, summoner_id, champion_id, region):
+        url = "https://" + region + ".api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/"+ summoner_id + "/by-champion/" +str(champion_id) + "?api_key="
+        y = self.get_from_api(url)
+        return y["championPoints"]
+    
+    def add_puuids(self, df, region, verbose = False):
+        puuids = []
+        for i in tqdm(range(len(df)), disable = not verbose):
+            y = self.get_from_api("https://"+region+".api.riotgames.com/lol/summoner/v4/summoners/"+ df.loc[i, "summonerId"] + "?api_key=")
+            puuids.append(y["puuid"])
+        df["puuid"] = puuids
+        return df
+    
+    def get_summoner_ids(self, ranks, divs, region, nb_pages = 1, verbose = False):
+        pages = range(1,nb_pages+1)[::-1]
+        dfs = []
+        for rank in ranks:
+            if verbose: 
+                print("Currently fetchin rank :", rank)
+            for div in tqdm(divs, disable = not verbose):
+                temp_dfs = []
+                for page in pages:
+                    j = self.get_from_api("https://"+ region +".api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/" + rank + "/" + div + "?page="+ str(page) + "&api_key=")
+                    df = pd.DataFrame(j)
+                    temp_dfs.append(df)
+                final_df = pd.concat(temp_dfs, axis = 0)
+                final_df.to_csv("../data/matches/matches_data_"+ rank +"_"+div+".csv", index  = False)
+                dfs.append(final_df)
+        return dfs
+    
+verbose = True
+region = "euw1"
+region_alias = "europe"
+api = API(api_key)
+for i in range(len(ranks)):
+    if verbose: 
+        print("Gathering random summoner ids")
+    dfs = api.get_summoner_ids([ranks[i]], divs, region, nb_pages = 5, verbose = verbose)
+    rank_df = pd.concat(dfs, axis = 0)
+    if verbose:
+        print("Gathering puuids")
+    rank_df = api.add_puuids(rank_df, region, verbose = verbose)
+    if verbose:
+        print("Gathering match ids")
+    match_ids = api.get_match_ids(rank_df["puuid"], region_alias, verbose = verbose)
+    L = []
+    if verbose:
+        print("Gathering summoners ids and champions")
+    for i in tqdm(range(len(match_ids)), disable = not verbose):
+        L.append(api.get_match_infos(match_ids[i], region_alias))
+    final_df = pd.DataFrame(L)
+    
+    if verbose:
+        print("Gathering champion points")
+    champion_points = [[] for i in range(10)]
+    for i in tqdm(range(len(final_df)), disable = not verbose):
+        for j in range(10):
+            cpoint = api.get_mastery(final_df["summonerId_"+str(j)][i], final_df["champ_"+str(j)][i], region)
+            champion_points[j].append(cpoint)
+    for j in range(10):
+        final_df["championPoints_"+str(j)] = champion_points[j]
+    final_df.to_excel("data_points_" + ranks[i] + ".csv", index = False)
